abe5ba8c591865f5c9f4b83de175bacd209a66c5
Ayoseun <48213053+Ayoseun@users.noreply.github.com>
2020-06-06 13:44:11 +0100

    Create HNG-01134.php
    this is my new file please delete HNG-01134.js

---
+++ scripts/scripts/HNG-01134.php
@@ -0,0 +1,8 @@
+<?php
+        $name = "Ayo Solomon";
+        $hngid = "HNG-01134";
+        $lang = "PHP";
+        $email = "ayoseunsolomon@gmail";
+        $display = "Hello World, this is ".$name." with HNGi7 ID ".$hngid." using ".$lang." for stage 2 task. ".$email."";
+        echo $display;
+?>
